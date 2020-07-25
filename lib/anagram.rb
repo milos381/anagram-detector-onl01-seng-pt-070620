5bbccce7b438762cf5c357a1662258ba08d7592d
Milos Popovic <milos_popovic@outlook.com>
2020-07-25 02:02:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+  def match(anagrams)
+    new_arr = []
+    anagrams.each do |words|
+      if words.split("").sort == @word.split("").sort
+        new_arr << words
+      end
+    end
+    new_arr
+  end
+
+end
