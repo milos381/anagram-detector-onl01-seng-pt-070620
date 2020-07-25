0d1e538c424d69237f5457ab0615c767c9af7626
Milos Popovic <milos_popovic@outlook.com>
2020-07-25 00:31:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
     @word = word
   end
   def match(anagrams)
-    anagrams.map do |words|
+    anagrams.each do |words|
       if words.split("") == @word
         return words
       end
